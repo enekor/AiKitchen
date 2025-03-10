85025e08b761c30442f4299a184b565e32a245bc
Eneko <eneko12rebollo@gmail.com>
2025-03-10 09:18:49 +0100

    arreglo de scrollview en favoritos

--- lib/home/favourites/favourites.dart
+++ lib/home/favourites/favourites.dart
@@ -78,51 +78,53 @@ class _FavouritesState extends State<Favourites> {
         } else {
           return Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Wrap(
-              alignment: WrapAlignment.center,
-              children:
-                  _recetasFavoritas
-                      .map(
-                        (receta) => Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Spacer(),
-                                  Text(
-                                    receta.nombre,
-                                    style:
-                                        Theme.of(context).textTheme.titleLarge,
-                                  ),
-                                  SizedBox(height: 10),
-                                  Text(receta.descripcion),
-                                  Spacer(),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      IconButton(
-                                        icon: const Icon(Icons.delete),
-                                        onPressed: () {
-                                          removeFavRecipe(receta);
-                                          setState(() {});
-                                        },
-                                      ),
-                                      IconButton(
-                                        onPressed: () => openRecipe(receta),
-                                        icon: Icon(Icons.open_in_new_rounded),
-                                      ),
-                                    ],
-                                  ),
-                                  // Add more widgets to display other properties of receta
-                                ],
+            child: SingleChildScrollView(
+              child: Wrap(
+                alignment: WrapAlignment.center,
+                children:
+                    _recetasFavoritas
+                        .map(
+                          (receta) => Card(
+                            color: Theme.of(context).colorScheme.secondary.withAlpha(80),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Spacer(),
+                                    Text(
+                                      receta.nombre,
+                                      style:
+                                          Theme.of(context).textTheme.titleLarge,
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(receta.descripcion),
+                                    Spacer(),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        IconButton(
+                                          icon: const Icon(Icons.delete),
+                                          onPressed: () {
+                                            removeFavRecipe(receta);
+                                            setState(() {});
+                                          },
+                                        ),
+                                        IconButton(
+                                          onPressed: () => openRecipe(receta),
+                                          icon: Icon(Icons.open_in_new_rounded),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      )
-                      .toList(),
+                        )
+                        .toList(),
+              ),
             ),
           );
         }
