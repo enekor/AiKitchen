a91dd05b37927683aa2cd4d38e8c14e39b69fc24
Eneko Rebollo <enekorebollo@gmail.com>
2025-04-29 16:26:31 +0200

    -mpoder reiniciar la lista de la compra por ia

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b205b6b48a2ef047cc08ac7242939f19621224
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "725759372"
+    serviceWorkerVersion: "1594541892"
   }
 });
