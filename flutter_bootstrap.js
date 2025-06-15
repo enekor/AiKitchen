8198ee062ab4e3696482773dbe699bbb3e060117
Eneko Rebollo <enekorebollo@gmail.com>
2025-06-15 19:14:33 +0200

    mejoras de home y recreacion de lista de la compra

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b205b6b48a2ef047cc08ac7242939f19621224
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4218882592"
+    serviceWorkerVersion: "4247753310"
   }
 });
